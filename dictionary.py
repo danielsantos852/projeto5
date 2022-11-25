a1484d4fca8787bdd8a525d990d9ff966436d37d
Daniel Rocha <danielsantos852@gmail.com>
2022-11-25 15:36:25 -0300

    Versão final?

--- dictionary.py
+++ dictionary.py
@@ -14,17 +14,17 @@ from csv import DictReader, DictWriter  # ...for CSV file-handling
 from difflib import get_close_matches   # ...for word searching
 
 # Define constants
-PATH_TO_CSV_FILE = "dictionary.csv"     # Path to dictionary
-
+FILE_PATH = "dictionary.csv"            # Path to dictionary
+MENU_OPTIONS = [1,2,3,4,5,6,7,8,0]      # Menu options
 
 def main():
     
     ''' Main function. '''
 
-    # Load dictionary from CSV file
-    aurelio = file_load(path=PATH_TO_CSV_FILE)
+    # Load dictionary into memory
+    aurelio = file_load(path=FILE_PATH)
 
-    # Create infinite loop
+    # Start infinite loop
     while True:
 
         # Clear the screen
@@ -37,39 +37,36 @@ def main():
         option = get_user_option()
 
         # While option not valid:
-        while option not in [1, 2, 3, 4, 5, 6, 7, 8, 0]:
-
+        while option not in MENU_OPTIONS:
+            
             # Re-prompt user for option
             option = get_user_option()
-
-        # Print a blank line
-        print()
-
-        # If "List all words" selected:
+        
+        # If "Check word-definition" selected:
         if option == 1:
+
+            # Call print key and value function
+            dictionary_print_key_and_value(dictionary=aurelio)
+        
+        # Else, if "List all words" selected:
+        elif option == 2:
 
             # Call list all keys function
             dictionary_keys_list_all(dictionary=aurelio)
 
         # Else, if "Search word by character" selected:
-        elif option == 2:
+        elif option == 3:
 
-            # Call search words by character function
+            # Call search keys by character function
             dictionary_keys_search_by_character(dictionary=aurelio)
 
         # Else, if "Search word by syllable" selected:
-        elif option == 3:
-
-            # Call search words by syllable function
-            dictionary_keys_search_by_syllable(dictionary=aurelio)
-
-        # Else, if "Search whole word" selected:
         elif option == 4:
 
-            # Call search whole word function
-            dictionary_keys_search_whole_word(dictionary=aurelio)
+            # Call search keys by syllable function
+            dictionary_keys_search_by_syllable(dictionary=aurelio)
 
-        # Else, if "Add new word-meaning" selected:
+        # Else, if "Add new word-definition" selected:
         elif option == 5:
 
             # Call add key function
@@ -81,7 +78,7 @@ def main():
             # Call update key function
             dictionary_key_update(dictionary=aurelio)
 
-        # Else, if "Update meaning" selected:
+        # Else, if "Update definition" selected:
         elif option == 7:
 
             # Call update value function
@@ -91,13 +88,13 @@ def main():
         elif option == 8:
 
             # Call delete key function
-            dictionary_key_remove(dictionary=aurelio)
+            dictionary_key_delete(dictionary=aurelio)
 
         # Else ("Exit program" selected):
         else:
 
             # Save dictionary to csv file
-            file_save(aurelio, PATH_TO_CSV_FILE)
+            file_save(aurelio, FILE_PATH)
 
             # Exit program
             exit('Exiting program...')
@@ -115,15 +112,15 @@ def print_menu() -> None:
 ===============================
 | ----  DICTIONARY MENU  ---- |
 ===============================
-1. List all words
-2. Search word (by character)
-3. Search word (by syllable)
-4. Search whole word
+1. Check word-definition
+2. List all words
+3. Search word by character
+4. Search word by syllable
 
-5. Add new word (and meaning)
-6. Update a word
-7. Update a meaning
-8. Delete a word (and meaning)
+5. Add new word-definition
+6. Update word
+7. Update definition
+8. Delete word-definition
 
 0. Exit program
 ===============================
@@ -151,32 +148,83 @@ def get_user_option() -> int:
     return option
 
 
+def dictionary_print_key_and_value(dictionary: dict) -> None:
+    
+    ''' Prints the key and value of input dict.
+        Returns nothing. '''
+    
+    # Print a header
+    print('\nCHECK WORD-DEFINITION:', end='\n\n')
+    
+    # Start infinite loop
+    while True:
+        
+        # Prompt user for key
+        key = input('Input word: ')
+        
+        # If word is in dictionary
+        if key in dictionary:
+            
+            # Print word and definition
+            print(f'{key}: {dictionary[key]}')
+            
+            # Exit function
+            return None
+            
+        # Else
+        else:
+            
+            # Print "word not found" message
+            print(f'Word {key} not found in dictionary.')
+            
+            # Prompt user for retry
+            answer = input('Try again? ').strip().lower()
+            
+            # If answer is yes:
+            if answer in ['y', 'yes']:
+                
+                # Skip loop to next lap
+                continue
+                
+            # Exit function
+            return None
+
+
 def dictionary_keys_list_all(dictionary: dict) -> None:
     
     ''' Prints all keys of input dict on screen. 
         Returns nothing. '''
 
     # Print a header
-    print('WORDS IN DICTIONARY:')
+    print('\nLIST OF WORDS:')
 
     # For each key in dictionary:
     for key in sorted(dictionary):
-        n_key = str(key).capitalize()
+        
         # Print key
-        print(f'- {n_key}')
+        print(f'- {key}')
+        
+    # Ask if user wants to check a word's meaning
+    answer = input("\nCheck a word's meaning? ").strip().lower()
+    
+    # If answer is yes:
+    if answer in ['yes', 'y']:
+        
+        # Call print key and value
+        dictionary_print_key_and_value(dictionary=dictionary)
 
 
 def dictionary_keys_search_by_character(dictionary: dict) -> None:
     
     ''' Searches a key by character in input dict. 
-        Prints a sorted list of keys that match search character. 
+        Prints list of matching keys. 
         Returns nothing. '''
 
     # Print a header
-    print('SEARCH WORD BY CHARACTER:',end='\n\n')
+    print('\nSEARCH WORD BY CHARACTER:',end='\n\n')
 
     # Prompt user for search character
-    character = input('Input character for search: ').strip().lower()
+    character = input('Input search character: ').strip().lower()
 
     # While character not valid:
     while (len(character)!=1) or (character.isalpha()==False):
@@ -185,7 +233,7 @@ def dictionary_keys_search_by_character(dictionary: dict) -> None:
         print('ERROR: Input must be single, alphabetic character. ',end='')
 
         # Re-prompt user for search letter
-        character = input('Input character for search: ').strip().lower()
+        character = input('Input search character: ').strip().lower()
 
     # Create list of words that match search letter
     results = sorted([key for key in dictionary if key[0]==character])
@@ -194,7 +242,7 @@ def dictionary_keys_search_by_character(dictionary: dict) -> None:
     if len(results)==0:
         
         # Print "No match" message
-        print(f'No words match the search "{character.upper()}".')
+        print(f'No words match the search: {character.upper()}.')
         
         # Exit function
         return None
@@ -206,57 +254,68 @@ def dictionary_keys_search_by_character(dictionary: dict) -> None:
     for key in results:
         
         # Print key
-        print(key)
+        print(f'- {key}')
+            
+    # Ask if user wants to check a word's meaning
+    answer = input("\nCheck a word's meaning? ").strip().lower()
+    
+    # If answer is yes:
+    if answer in ['yes', 'y']:
+        
+        # Call print key and value
+        dictionary_print_key_and_value(dictionary=dictionary)
 
 
 def dictionary_keys_search_by_syllable(dictionary: dict) -> None:
     
-    ''' Searches a key by syllable in input dict. 
+    ''' Searches a key by syllable in input dict.
+        Prints list of matching keys. 
         Returns nothing. '''
 
     # Print a header
-    print('SEARCH WORD BY SYLLABLE:',end='\n\n')
+    print('\nSEARCH WORD BY SYLLABLE:',end='\n\n')
 
-    letter = input('Input syllable to search: ').strip().lower()
-    words = [key for key, value in dictionary.items() if letter in key]
-    words.sort()
-    n_words = [word.capitalize() for word in words]
-    new_line = "\n"
-    if len(words) != 0:
-        print(f"\nThese are the results:{new_line}{new_line.join(n_words)}")
-    else:
-        print("Sorry we couldn't find any word that matched your search.")
+    # Prompt user for search syllable
+    syllable = input('Input search syllable: ').strip().lower()
+    
+    # While syllable not valid
+    while (len(syllable)<2) or (len(syllable)>5) or (syllable.isalpha()==False):
 
+        # Print error message
+        print('ERROR: Input must be 2-5, alphabetic characters long. ',end='')
+        
+        # Re-prompt user for search syllable
+        syllable = input('Input search syllable: ').strip().lower()
+    
+    # Create list of results
+    results = sorted([key for key in dictionary if syllable==key[0:len(syllable)-1]])
 
-def dictionary_keys_search_whole_word(dictionary: dict) -> None:
+# If no words match the search:
+    if len(results)==0:
+        
+        # Print "No match" message
+        print(f'No words match the search: {syllable.upper()}.')
+        
+        # Exit function
+        return None
+    
+    # Print "search results" header
+    print('\nSEARCH RESULTS: ')
+    
+    # For each key in results list:
+    for key in results:
+        
+        # Print key
+        print(f'- {key}')
 
-    # ADD A DOCSTRING
-
-    # Print a header
-    print('SEARCH WHOLE WORD:',end='\n\n')
-
-    # Prompt user for search key
-    key = input("Input word to search: ").strip().lower()
-    n_key = str(key.capitalize())
-    # If word is already added to dict, just print
-    if key in dictionary.keys():
-        # Fazer o print com word -> meaning melhor
-        print(n_key, ":", dictionary[key])
-
-    elif len(get_close_matches(key, dictionary.keys())) > 0:
-        ans = input("Did you mean % s instead? Enter Y if yes, or N if no: " %
-                    get_close_matches(key, dictionary.keys())[0].capitalize())
-        ans = ans.lower()
-        if ans == "y":
-            print(get_close_matches(key, dictionary.keys())[0].capitalize(
-            ), ":", dictionary[get_close_matches(key, dictionary.keys())[0]])
-        elif ans == "n":
-            print(
-                "The word doesn't exist in our database. Please check again or add the word to the database.")
-        else:
-            print("We didn't understand your entry.")
-    else:
-        print("The word doesn't exist in our database. Please check again or add the word to the database.")
+    # Ask if user wants to check a word's meaning
+    answer = input("\nCheck a word's meaning? ").strip().lower()
+    
+    # If answer is yes:
+    if answer in ['yes', 'y']:
+        
+        # Call print key and value
+        dictionary_print_key_and_value(dictionary=dictionary)
 
 
 def dictionary_key_add(dictionary: dict) -> dict:
@@ -264,11 +323,14 @@ def dictionary_key_add(dictionary: dict) -> dict:
     ''' Adds a new key:value pair to input dict. 
         Returns updated dict. '''
 
+    # Print a header
+    print('\nADD WORD-DEFINITION: ', end='\n\n')
+
     # Prompt user for new key
     key = input("Input new word: ").strip().lower()
 
     # Prompt user for new key's value
-    value = input("Input new word's meaning: ").strip().capitalize()
+    value = input("Input new word's definition: ").strip().capitalize()
 
     # Add new key:value pair to dictionary
     dictionary[key] = value
@@ -282,6 +344,9 @@ def dictionary_key_update(dictionary: dict) -> dict:
     ''' Copies value from old key to new key in input dict.
         Deletes old key.
         Returns updated dict.'''
+
+    # Print a header
+    print('\nUPDATE WORD:', end='\n\n')
 
     # Prompt user for key to be replaced
     key_old = input('Which word to replace? ')
@@ -306,13 +371,41 @@ def dictionary_key_update(dictionary: dict) -> dict:
     return dict
 
 
-def dictionary_key_remove(dictionary: dict) -> dict:
+def dictionary_value_update(dictionary: dict) -> dict:
+    
+    ''' Replaces the value of a key in input dict.
+        Returns updated dict. '''
+
+    # Print a header
+    print('\nUPDATE DEFINITION:', end='\n\n')
+
+    # Prompt user for key whose value will be updated
+    key = input('Update the definition of which word? ').strip().lower()
+
+    # While key not in dict:
+    while key not in dictionary:
+
+        # Re-prompt user for key
+        key = input(f'Word "{key}" is not in dictionary. '
+                    'Update the definition of which word? ')
+
+    # Prompt user for new definition
+    value = input(f'Input new definition for "{key}": ')
+
+    # Update value for key in dictionary
+    dictionary[key] = value
+
+    # Return dictionary
+    return dictionary
+
+
+def dictionary_key_delete(dictionary: dict) -> dict:
     
     ''' Deletes a key-value pair from input dict.
         Returns updated dict. '''
 
     # Print a header
-    print('DELETE A WORD:')
+    print('\nDELETE WORD-DEFINITION:', end='\n\n')
 
     # Prompt user for key to be deleted
     key = input('Which word to delete? ').strip().lower()
@@ -331,37 +424,9 @@ def dictionary_key_remove(dictionary: dict) -> dict:
     return dictionary
 
 
-def dictionary_value_update(dictionary: dict) -> dict:
-    
-    ''' Replaces the value of a key in input dict.
-        Returns updated dict. '''
-
-    # Print a header
-    print('UPDATE A MEANING:')
-
-    # Prompt user for key whose value will be updated
-    key = input('Update the meaning of which word? ').strip().lower()
-
-    # While key not in dict:
-    while key not in dictionary:
-
-        # Re-prompt user for key
-        key = input(f'Word "{key}" is not in dictionary. '
-                    'Update the meaning of which word? ')
-
-    # Prompt user for new meaning
-    meaning = input(f'Input new meaning for "{key}": ')
-
-    # Update value for key in dictionary
-    dictionary[key] = meaning
-
-    # Return dictionary
-    return dictionary
-
-
 def file_load(path: str) -> dict:
     
-    ''' Loads the contents of a CSV file to a dict.
+    ''' Loads the contents of a CSV file into a new dict.
         Returns created dict'''
 
     # Create an empty dict
@@ -373,11 +438,11 @@ def file_load(path: str) -> dict:
         # Get a dict reader to the file
         reader = DictReader(file)
 
-        # For each word-meaning pair in the file...
+        # For each word-definition pair in the file...
         for row in reader:
 
-            # Add word-meaning as key:value in dict
-            dictionary[row['word']] = row['meaning']
+            # Add word-definition as key:value in dict
+            dictionary[row['word']] = row['definition']
 
     # Return dict
     return dictionary
@@ -392,7 +457,7 @@ def file_save(dictionary: dict, path: str) -> None:
     with open(path, 'w') as file:
 
         # Get a dict writer to the file
-        writer = DictWriter(file, fieldnames=['word', 'meaning'])
+        writer = DictWriter(file, fieldnames=['word', 'definition'])
 
         # Write fieldnames as headers in csv file
         writer.writeheader()
@@ -401,7 +466,7 @@ def file_save(dictionary: dict, path: str) -> None:
         for key in dictionary:
 
             # ...copy key:value pair from dictionary to csv file
-            writer.writerow({'word': key, 'meaning': dictionary[key]})
+            writer.writerow({'word': key, 'definition': dictionary[key]})
 
 
 def screen_clear() -> None:
